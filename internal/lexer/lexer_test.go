818c39dd578cfefdee14451a67cb07feae428ec4
Piotr Kowalczuk <p.kowalczuk@travelaudience.com>
2016-11-06 20:37:18 +0100

    lexer.TestLexer replaced by lexer.TestLexer_ConsumeFloat for simplification

--- internal/lexer/lexer_test.go
+++ internal/lexer/lexer_test.go
@@ -1,83 +1,59 @@
 package lexer_test
 
 import (
-	"fmt"
-	"reflect"
 	"strings"
 	"testing"
 	"text/scanner"
 
+	"strconv"
+
+	"github.com/engoengine/math"
 	"github.com/neelance/graphql-go/internal/lexer"
 )
 
-func TestLexer(t *testing.T) {
+func TestLexer_ConsumeFloat(t *testing.T) {
 	cases := map[string]struct {
 		given    string
-		expected []interface{}
+		expected float64
 	}{
-		"array of strings": {
-			given:    `String ["a","b","c",""]`,
-			expected: []interface{}{"a", "b", "c", ""},
+		"zero": {
+			given:    "0",
+			expected: 0.0,
 		},
-		"array of strings with spaces": {
-			given:    `String ["a", "b" ,      "c",""    ]`,
-			expected: []interface{}{"a", "b", "c", ""},
+		"regular": {
+			given:    "1.5",
+			expected: 1.5,
 		},
-		"array of integers": {
-			given: `Int	[1,2,44,550125]`,
-			expected: []interface{}{1, 2, 44, 550125},
+		"max-int64": {
+			given:    strconv.FormatInt(math.MaxInt64, 10),
+			expected: float64(math.MaxInt64),
 		},
-		"array of floats": {
-			given: `Float	[0,0.5,1,1.2,55,4.012]`,
-			expected: []interface{}{0.0, 0.5, 1.0, 1.2, 55.0, 4.012},
+		"max-float64": {
+			given:    strconv.FormatFloat(math.MaxFloat64, 'g', 100000000, 64),
+			expected: math.MaxFloat64,
+		},
+		"smallest-nonzero-float64": {
+			given:    strconv.FormatFloat(math.SmallestNonzeroFloat64, 'g', 100000000, 64),
+			expected: math.SmallestNonzeroFloat64,
 		},
 	}
-	readArray := func(l *lexer.Lexer, fn func(*lexer.Lexer)) {
-		l.ConsumeToken('[')
-		for l.Peek() != ']' {
-			fn(l)
-			if l.Peek() == ',' {
-				l.ConsumeToken(',')
-			}
-		}
-		l.ConsumeToken(']')
-	}
+
 	for hint, c := range cases {
 		t.Run(hint, func(t *testing.T) {
-			s := &scanner.Scanner{
-				Mode: scanner.ScanIdents | scanner.ScanInts | scanner.ScanFloats | scanner.ScanStrings,
-			}
+			s := &scanner.Scanner{}
 			s.Init(strings.NewReader(c.given))
 			l := lexer.New(s)
-			got := []interface{}{}
+			var got float64
 
 			err := l.CatchSyntaxError(func() {
-				for l.Peek() != scanner.EOF {
-					switch keyword := l.ConsumeIdent(); keyword {
-					case "String":
-						readArray(l, func(l *lexer.Lexer) {
-							got = append(got, l.ConsumeString())
-						})
-					case "Int":
-						readArray(l, func(l *lexer.Lexer) {
-							got = append(got, l.ConsumeInt())
-						})
-					case "Float":
-						readArray(l, func(l *lexer.Lexer) {
-							got = append(got, l.ConsumeFloat())
-						})
-					default:
-						l.SyntaxError(fmt.Sprintf(`unexpected %q, expecting "String", "Float", "Integer"`, keyword))
-					}
-				}
+				got = l.ConsumeFloat()
 			})
 			if err != nil {
 				t.Fatalf("unexpected error: %s", err.Error())
 			}
-			if !reflect.DeepEqual(c.expected, got) {
-				t.Errorf("wrong output, expected %v but got %v", c.expected, got)
+			if c.expected != got {
+				t.Errorf("wrong output, expected %f but got %f", c.expected, got)
 			}
 		})
 	}
-
 }
