2f5f0448ab662d8301dc54d74d59656bfce3eed6
Mark Saward <saward@users.noreply.github.com>
2016-11-10 14:39:20 +1100

    Update relay.go
    Fixed to handle empty strings and json formatted variable lists.

--- relay/relay.go
+++ relay/relay.go
@@ -50,16 +50,28 @@ type Handler struct {
 
 func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	var params struct {
-		Query         string                 `json:"query"`
-		OperationName string                 `json:"operationName"`
-		Variables     map[string]interface{} `json:"variables"`
+		Query         string `json:"query"`
+		OperationName string `json:"operationName"`
+		Variables     string `json:"variables"`
 	}
+
 	if err := json.NewDecoder(r.Body).Decode(&params); err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	response := h.Schema.Exec(r.Context(), params.Query, params.OperationName, params.Variables)
+	var parsedVariables map[string]interface{}
+
+	if len(params.Variables) > 0 {
+		if err := json.Unmarshal([]byte(params.Variables), &parsedVariables); err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+	} else {
+		parsedVariables = make(map[string]interface{})
+	}
+
+	response := h.Schema.Exec(r.Context(), params.Query, params.OperationName, parsedVariables)
 	responseJSON, err := json.Marshal(response)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
