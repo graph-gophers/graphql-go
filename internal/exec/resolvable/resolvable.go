fa851eca463e3927194ee95b8a3aa06265ccad6d
Alif Rachmawadi <code@subosito.com>
2017-04-04 12:57:37 +0700

    add supports for snake case

--- internal/exec/resolvable/resolvable.go
+++ internal/exec/resolvable/resolvable.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"reflect"
 	"strings"
+	"unicode"
 
 	"github.com/neelance/graphql-go/internal/common"
 	"github.com/neelance/graphql-go/internal/schema"
@@ -333,7 +334,7 @@ func (b *execBuilder) makeFieldExec(typeName string, f *schema.Field, m reflect.
 
 func findMethod(t reflect.Type, name string) int {
 	for i := 0; i < t.NumMethod(); i++ {
-		if strings.EqualFold(name, t.Method(i).Name) {
+		if strings.EqualFold(name, t.Method(i).Name) || strings.EqualFold(camelize(name), camelize(t.Method(i).Name)) {
 			return i
 		}
 	}
@@ -345,4 +346,70 @@ func unwrapNonNull(t common.Type) (common.Type, bool) {
 		return nn.OfType, true
 	}
 	return t, false
+}
+
+func camelize(s string) string {
+	var ws string
+
+	for _, w := range strings.Split(s, "_") {
+		if len(w) == 0 {
+			continue
+		}
+
+		if u := strings.ToUpper(w); commonInitialisms[u] {
+			ws += u
+			continue
+		}
+
+		z := []rune(w)
+		z[0] = unicode.ToUpper(z[0])
+		ws += string(z)
+	}
+
+	return ws
+}
+
+// commonInitialisms is a set of common initialisms.
+// Only add entries that are highly unlikely to be non-initialisms.
+// For instance, "ID" is fine (Freudian code is rare), but "AND" is not.
+// Taken from: https://github.com/golang/lint/blob/206c0f020eba0f7fbcfbc467a5eb808037df2ed6/lint.go#L731
+var commonInitialisms = map[string]bool{
+	"ACL":   true,
+	"API":   true,
+	"ASCII": true,
+	"CPU":   true,
+	"CSS":   true,
+	"DNS":   true,
+	"EOF":   true,
+	"GUID":  true,
+	"HTML":  true,
+	"HTTP":  true,
+	"HTTPS": true,
+	"ID":    true,
+	"IP":    true,
+	"JSON":  true,
+	"LHS":   true,
+	"QPS":   true,
+	"RAM":   true,
+	"RHS":   true,
+	"RPC":   true,
+	"SLA":   true,
+	"SMTP":  true,
+	"SQL":   true,
+	"SSH":   true,
+	"TCP":   true,
+	"TLS":   true,
+	"TTL":   true,
+	"UDP":   true,
+	"UI":    true,
+	"UID":   true,
+	"UUID":  true,
+	"URI":   true,
+	"URL":   true,
+	"UTF8":  true,
+	"VM":    true,
+	"XML":   true,
+	"XMPP":  true,
+	"XSRF":  true,
+	"XSS":   true,
 }
