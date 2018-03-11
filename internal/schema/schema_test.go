f880e30f4893ca0cf6786496cf9dd7d71044731f
Tony Ghita <ghita71@gmail.com>
2018-03-11 13:40:49 -0700

    Improve parseObjectDef() test

--- internal/schema/schema_test.go
+++ internal/schema/schema_test.go
@@ -5,67 +5,106 @@ import (
 	"testing"
 	"text/scanner"
 
+	"github.com/graph-gophers/graphql-go/errors"
 	"github.com/graph-gophers/graphql-go/internal/common"
 )
 
-type testCase struct {
-	description string
-	declaration string
-	expected    *Object
-}
-
-func TestParseObjectDeclaration(t *testing.T) {
-	tests := []testCase{
-		{
-			"allows '&' separator",
-			"Alien implements Being & Intelligent { name: String, iq: Int }",
-			&Object{
-				Name:           "Alien",
-				interfaceNames: []string{"Being", "Intelligent"},
-			},
-		},
-		{
-			"allows legacy ',' separator",
-			"Alien implements Being, Intelligent { name: String, iq: Int }",
-			&Object{
-				Name:           "Alien",
-				interfaceNames: []string{"Being", "Intelligent"},
-			},
-		},
+// TestParseObjectDef tests the logic for parsing object types from the schema definition as
+// written in `parseObjectDef()`.
+func TestParseObjectDef(t *testing.T) {
+	type testCase struct {
+		description string
+		definition  string
+		expected    *Object
+		err         *errors.QueryError
 	}
 
-	setup := func(schema string) *common.Lexer {
+	tests := []testCase{{
+		description: "Parses type inheriting single interface",
+		definition:  "Hello implements World { field: String }",
+		expected:    &Object{Name: "Hello", interfaceNames: []string{"World"}},
+	}, {
+		description: "Parses type inheriting multiple interfaces",
+		definition:  "Hello implements Wo & rld { field: String }",
+		expected:    &Object{Name: "Hello", interfaceNames: []string{"Wo", "rld"}},
+	}, {
+		description: "Parses type inheriting multiple interfaces with leading ampersand",
+		definition:  "Hello implements & Wo & rld { field: String }",
+		expected:    &Object{Name: "Hello", interfaceNames: []string{"Wo", "rld"}},
+	}, {
+		description: "Allows legacy SDL interfaces",
+		definition:  "Hello implements Wo, rld { field: String }",
+		expected:    &Object{Name: "Hello", interfaceNames: []string{"Wo", "rld"}},
+	}}
+
+	setup := func(def string) *common.Lexer {
 		sc := &scanner.Scanner{
 			Mode: scanner.ScanIdents | scanner.ScanInts | scanner.ScanFloats | scanner.ScanStrings,
 		}
-		sc.Init(strings.NewReader(schema))
+		sc.Init(strings.NewReader(def))
 		return common.NewLexer(sc)
 	}
 
 	for _, test := range tests {
 		t.Run(test.description, func(t *testing.T) {
-			lex := setup(test.declaration)
 			var actual *Object
+			lex := setup(test.definition)
 
-			parse := func() { actual = parseObjectDeclaration(lex) }
-			if err := lex.CatchSyntaxError(parse); err != nil {
-				t.Fatal(err)
-			}
+			parse := func() { actual = parseObjectDef(lex) }
+			err := lex.CatchSyntaxError(parse)
 
-			if test.expected.Name != actual.Name {
-				t.Errorf("wrong object name: want %q, got %q", test.expected.Name, actual.Name)
-			}
-
-			if len(test.expected.interfaceNames) != len(actual.interfaceNames) {
-				t.Fatalf("wrong number of interface names: want %s, got %s", test.expected.interfaceNames, actual.interfaceNames)
-			}
-
-			for i, expectedName := range test.expected.interfaceNames {
-				actualName := actual.interfaceNames[i]
-				if expectedName != actualName {
-					t.Errorf("wrong interface name: want %q, got %q", expectedName, actualName)
-				}
-			}
+			compareErrors(t, test.err, err)
+			compareObjects(t, test.expected, actual)
 		})
+	}
+}
+
+func compareErrors(t *testing.T, expected, actual *errors.QueryError) {
+	t.Helper()
+
+	switch {
+	case expected != nil && actual != nil:
+		if expected.Message != actual.Message {
+			t.Fatalf("wanted error message %q, got %q", expected.Message, actual.Message)
+		}
+		// TODO: Check error locations are as expected.
+
+	case expected != nil && actual == nil:
+		t.Fatalf("missing expected error: %q", expected)
+
+	case expected == nil && actual != nil:
+		t.Fatalf("got unexpected error: %q", actual)
+	}
+}
+
+func compareObjects(t *testing.T, expected, actual *Object) {
+	t.Helper()
+
+	switch {
+	case expected == nil && expected == actual:
+		return
+	case expected == nil && actual != nil:
+		t.Fatalf("got an unexpected object: %#v", actual)
+	case expected != nil && actual == nil:
+		t.Fatalf("wanted non-nil object, got nil")
+	}
+
+	if expected.Name != actual.Name {
+		t.Errorf("wrong object name: want %q, got %q", expected.Name, actual.Name)
+	}
+
+	if len(expected.interfaceNames) != len(actual.interfaceNames) {
+		t.Fatalf(
+			"wrong number of interface names: want %s, got %s",
+			expected.interfaceNames,
+			actual.interfaceNames,
+		)
+	}
+
+	for i, expectedName := range expected.interfaceNames {
+		actualName := actual.interfaceNames[i]
+		if expectedName != actualName {
+			t.Errorf("wrong interface name: want %q, got %q", expectedName, actualName)
+		}
 	}
 }
