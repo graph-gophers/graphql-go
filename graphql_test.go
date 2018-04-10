a44cea399763f2d03b55dbf5e673357b0b1ce60b
Yingjie Run <loveKotiya@hotmail.com>
2018-04-10 19:02:59 +0800

    Add compatibility test

--- graphql_test.go
+++ graphql_test.go
@@ -1597,6 +1597,14 @@ func (r *inputResolver) NullableList(args struct{ Value *[]*struct{ V int32 } })
 	return &l
 }
 
+func (r *inputResolver) EnumString(args struct{ Value string }) string {
+	return args.Value
+}
+
+func (r *inputResolver) NullableEnumString(args struct{ Value *string }) *string {
+	return args.Value
+}
+
 func (r *inputResolver) Enum(args struct{ Value Enum }) Enum {
 	return args.Value
 }
@@ -1637,6 +1645,8 @@ func TestInput(t *testing.T) {
 			nullable(value: Int): Int
 			list(value: [Input!]!): [Int!]!
 			nullableList(value: [Input]): [Int]
+			enumString(value: Enum!): Enum!
+			nullableEnumString(value: Enum): Enum
 			enum(value: Enum!): Enum!
 			nullableEnum(value: Enum): Enum
 			recursive(value: RecursiveInput!): Int!
@@ -1672,6 +1682,9 @@ func TestInput(t *testing.T) {
 					list2: list(value: {v: 42})
 					nullableList1: nullableList(value: [{v: 41}, null, {v: 43}])
 					nullableList2: nullableList(value: null)
+					enumString(value: Option1)
+					nullableEnumString1: nullableEnum(value: Option1)
+					nullableEnumString2: nullableEnum(value: null)
 					enum(value: Option2)
 					nullableEnum1: nullableEnum(value: Option2)
 					nullableEnum2: nullableEnum(value: null)
@@ -1693,6 +1706,9 @@ func TestInput(t *testing.T) {
 					"list2": [42],
 					"nullableList1": [41, null, 43],
 					"nullableList2": null,
+					"enumString": "Option1",
+					"nullableEnumString1": "Option1",
+					"nullableEnumString2": null,
 					"enum": "Option2",
 					"nullableEnum1": "Option2",
 					"nullableEnum2": null,
