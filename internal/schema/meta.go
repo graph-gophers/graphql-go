4180da5a4a46d331450494a146109036b8259526
Jacob Lambert <jacob.lambert@getweave.com@Jacobs-MacBook-Pro.local>
2018-06-11 09:33:21 -0600

    simplify meta schema load

--- internal/schema/meta.go
+++ internal/schema/meta.go
@@ -1,28 +1,13 @@
 //go:generate go-bindata -ignore=\.go -pkg=schema -o=bindata.go ./...
 package schema
 
-import "bytes"
-
 var Meta *Schema
 
 func init() {
 	Meta = &Schema{} //bootstrap
 	Meta = New()
-	if err := Meta.Parse(metaSchemaString()); err != nil {
+	schemaBytes, _ := metaGraphqlBytes()
+	if err := Meta.Parse(string(schemaBytes)); err != nil {
 		panic(err)
 	}
-}
-
-func metaSchemaString() string {
-	buf := bytes.Buffer{}
-	for _, name := range AssetNames() {
-		b := MustAsset(name)
-		buf.Write(b)
-
-		if len(b) > 0 && b[len(b)-1] != '\n' {
-			buf.WriteByte('\n')
-		}
-	}
-
-	return buf.String()
 }
