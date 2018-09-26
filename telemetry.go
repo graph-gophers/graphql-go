9fd430748b14ed0e5fb7fd6818dddae44008448b
Tom Alcorn <tom@joincad.com>
2018-09-26 15:53:59 -0700

    Add telemetry.

---
+++ telemetry.go
@@ -0,0 +1,84 @@
+package graphql
+
+import (
+	"fmt"
+
+	"github.com/JoinCAD/graphql-go/errors"
+	"github.com/JoinCAD/graphql-go/internal/common"
+	"github.com/JoinCAD/graphql-go/internal/query"
+	"github.com/JoinCAD/graphql-go/internal/validation"
+)
+
+// LoggedOperation represents a summary of an operation suitable for concise
+// telemetry, for example in a web server context.
+type LoggedOperation struct {
+	Name      string `json:",omitempty"`
+	Type      query.OperationType
+	Variables map[string]string `json:",omitempty"`
+	Fields    []LoggedField     `json:",omitempty"`
+}
+
+// LoggedField represents a summary of a field.
+type LoggedField struct {
+	Name      string
+	Arguments map[string]string `json:",omitempty"`
+}
+
+func logField(field query.Field) LoggedField {
+	args := []common.Argument(field.Arguments)
+	var loggedArgs map[string]string
+	if len(args) > 0 {
+		loggedArgs = make(map[string]string)
+		for _, arg := range args {
+			loggedArgs[arg.Name.Name] = arg.Value.String()
+		}
+	}
+	return LoggedField{
+		Name:      field.Name.Name,
+		Arguments: loggedArgs,
+	}
+}
+
+func logOperations(doc *query.Document) []LoggedOperation {
+	ops := []*query.Operation(doc.Operations)
+	lops := make([]LoggedOperation, len(ops))
+	for i, op := range ops {
+		inputs := []*common.InputValue(op.Vars)
+		var args map[string]string
+		if len(inputs) > 0 {
+			args = make(map[string]string)
+			for _, input := range inputs {
+				args[input.Name.Name] = input.Default.String()
+			}
+		}
+
+		fields := make([]LoggedField, 0, len(op.Selections))
+		for _, sel := range op.Selections {
+			fmt.Printf("%+v\n", sel)
+			if field, ok := sel.(query.Field); ok {
+				fields = append(fields, logField(field))
+			} else if field, ok := sel.(*query.Field); ok {
+				fields = append(fields, logField(*field))
+			}
+		}
+
+		lops[i] = LoggedOperation{
+			Name:      op.Name.Name,
+			Type:      op.Type,
+			Variables: args,
+			Fields:    fields,
+		}
+	}
+	return lops
+}
+
+// ValidateAndLog validates the query and simultaneously produces a loggable
+// summary of the operations it contains.
+func (s *Schema) ValidateAndLog(queryString string) ([]*errors.QueryError, []LoggedOperation) {
+	doc, qErr := query.Parse(queryString)
+	if qErr != nil {
+		return []*errors.QueryError{qErr}, nil
+	}
+
+	return validation.Validate(s.schema, doc, s.maxDepth), logOperations(doc)
+}
---
+++ telemetry_test.go
@@ -0,0 +1,108 @@
+package graphql_test
+
+import (
+	"testing"
+
+	"github.com/JoinCAD/graphql-go"
+	"github.com/JoinCAD/graphql-go/internal/query"
+)
+
+func equalLoggedOperation(a, b graphql.LoggedOperation) bool {
+	if a.Type != b.Type {
+		return false
+	}
+	if a.Name != b.Name {
+		return false
+	}
+	for k, va := range a.Variables {
+		if vb, ok := b.Variables[k]; !ok || va != vb {
+			return false
+		}
+	}
+	for k, vb := range b.Variables {
+		if va, ok := a.Variables[k]; !ok || vb != va {
+			return false
+		}
+	}
+	if len(a.Fields) != len(b.Fields) {
+		return false
+	}
+	for i := range a.Fields {
+		if !equalLoggedField(a.Fields[i], b.Fields[i]) {
+			return false
+		}
+	}
+	return true
+}
+
+func equalLoggedField(a, b graphql.LoggedField) bool {
+	if a.Name != b.Name {
+		return false
+	}
+	for k, va := range a.Arguments {
+		if vb, ok := b.Arguments[k]; !ok || va != vb {
+			return false
+		}
+	}
+	for k, vb := range b.Arguments {
+		if va, ok := a.Arguments[k]; !ok || vb != va {
+			return false
+		}
+	}
+	return true
+}
+
+func TestValidateAndLog(t *testing.T) {
+	tests := []struct {
+		name        string
+		schema      *graphql.Schema
+		queryString string
+		want        []graphql.LoggedOperation
+		wantErrs    bool
+	}{
+		{
+			"basic test",
+			starwarsSchema,
+			`
+			{
+				hero {
+					id
+					name
+					friends {
+						name
+					}
+				}
+			}
+			`,
+			[]graphql.LoggedOperation{
+				{
+					Type:      query.Query,
+					Variables: map[string]string{},
+					Fields: []graphql.LoggedField{
+						{
+							Name:      "hero",
+							Arguments: map[string]string{},
+						},
+					},
+				},
+			},
+			false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			errs, got := tt.schema.ValidateAndLog(tt.queryString)
+			if !tt.wantErrs && len(errs) > 0 {
+				t.Errorf("Schema.ValidateAndLog() got errors %+v", errs)
+			}
+			if len(tt.want) != len(got) {
+				t.Errorf("Schema.ValidateAndLog() = %+v, want %+v", got, tt.want)
+			}
+			for i := range tt.want {
+				if !equalLoggedOperation(tt.want[i], got[i]) {
+					t.Errorf("Schema.ValidateAndLog()[%d] = %+v, want %+v", i, got[i], tt.want[i])
+				}
+			}
+		})
+	}
+}
