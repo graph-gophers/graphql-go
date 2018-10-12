f7c593a326c738cecf96b8d903215af6e53dd6b0
fadi-alkatut <44017676+fadi-alkatut@users.noreply.github.com>
2018-10-12 21:37:02 +1100

    Update testing.go
    - check for errors before expected result;
    - print more details about unexpected errors;
    - and only check for result if it's expected

--- gqltesting/testing.go
+++ gqltesting/testing.go
@@ -43,21 +43,22 @@ func RunTest(t *testing.T, test *Test) {
 		test.Context = context.Background()
 	}
 	result := test.Schema.Exec(test.Context, test.Query, test.OperationName, test.Variables)
-	// Verify JSON to avoid red herring errors.
-	got, err := formatJSON(result.Data)
-	if err != nil {
-		t.Fatalf("got: invalid JSON: %s\n response value is: [%s]", err, got)
-	}
-	want, err := formatJSON([]byte(test.ExpectedResult))
-	if err != nil {
-		t.Fatalf("want: invalid JSON: %s\n expected value is: [%s]", err, want)
-	}
 	checkErrors(t, test.ExpectedErrors, result.Errors)
-
-	if !bytes.Equal(got, want) {
-		t.Logf("got:  %s", got)
-		t.Logf("want: %s", want)
-		t.Fail()
+	if test.ExpectedResult != "" {
+		// Verify JSON to avoid red herring errors.
+		got, err := formatJSON(result.Data)
+		if err != nil {
+			t.Fatalf("got: invalid JSON: %s\n response value is: [%s]", err, got)
+		}
+		want, err := formatJSON([]byte(test.ExpectedResult))
+		if err != nil {
+			t.Fatalf("want: invalid JSON: %s\n expected value is: [%s]", err, want)
+		}
+		if !bytes.Equal(got, want) {
+			t.Logf("got:  %s", got)
+			t.Logf("want: %s", want)
+			t.Fail()
+		}
 	}
 }
 
@@ -75,24 +76,19 @@ func formatJSON(data []byte) ([]byte, error) {
 
 func checkErrors(t *testing.T, expected, actual []*errors.QueryError) {
 	expectedCount, actualCount := len(expected), len(actual)
-
 	if expectedCount != actualCount {
-		t.Fatalf("unexpected number of errors: got %d, want %d", actualCount, expectedCount)
+		t.Fatalf("unexpected number of errors: got %d, want %d\nerror(s):\n%+v", actualCount, expectedCount, actual)
 	}
-
 	if expectedCount > 0 {
 		for i, want := range expected {
 			got := actual[i]
-
 			if !reflect.DeepEqual(got, want) {
 				t.Fatalf("unexpected error: got %+v, want %+v", got, want)
 			}
 		}
-
 		// Return because we're done checking.
 		return
 	}
-
 	for _, err := range actual {
 		t.Errorf("unexpected error: '%s'", err)
 	}
