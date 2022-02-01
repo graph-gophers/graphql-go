aa6442974c9f6c0731faa23694069f0d03857b08
Pavel Nikolov <pavelnikolov@users.noreply.github.com>
2022-02-01 20:58:29 +1100

    Refactor trace package
    Remove dependency for graphql-go on OpenTracing and OpenTelemetry except
     where those tracers are explicitly configured for use.

--- trace/trace_test.go
+++ trace/trace_test.go
@@ -3,13 +3,40 @@ package trace_test
 import (
 	"testing"
 
+	"github.com/graph-gophers/graphql-go"
+	"github.com/graph-gophers/graphql-go/errors"
+	"github.com/graph-gophers/graphql-go/example/starwars"
 	"github.com/graph-gophers/graphql-go/trace"
+	"github.com/graph-gophers/graphql-go/trace/tracer"
 )
 
 func TestInterfaceImplementation(t *testing.T) {
-	var _ trace.ValidationTracerContext = &trace.OpenTelemetryTracer{}
-	var _ trace.Tracer = &trace.OpenTelemetryTracer{}
+	var _ tracer.ValidationTracer = &trace.OpenTracingTracer{}
+	var _ tracer.Tracer = &trace.OpenTracingTracer{}
 
-	var _ trace.ValidationTracerContext = &trace.OpenTracingTracer{}
-	var _ trace.Tracer = &trace.OpenTracingTracer{}
+	var _ tracer.ValidationTracer = &trace.NoopTracer{}
+	var _ tracer.Tracer = &trace.NoopTracer{}
+}
+
+func TestTracerOption(t *testing.T) {
+	_, err := graphql.ParseSchema(starwars.Schema, nil, graphql.Tracer(trace.OpenTracingTracer{}))
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+// MockVlidationTracer is a struct that implements the tracer.LegacyValidationTracer inteface.
+type MockValidationTracer struct{}
+
+func (MockValidationTracer) TraceValidation() func([]*errors.QueryError) {
+	return func([]*errors.QueryError) {}
+}
+
+func TestValidationTracer(t *testing.T) {
+	// test the legacy validation tracer interface (validating without using context) to ensure backwards compatibility
+	vt := MockValidationTracer{}
+	_, err := graphql.ParseSchema(starwars.Schema, nil, graphql.ValidationTracer(vt))
+	if err != nil {
+		t.Fatal(err)
+	}
 }
