3fa26351e386607cf168173f40ad163c7c83faa8
pavelnikolov <me@pavelnikolov.net>
2022-03-20 16:21:17 +0200

    Fix formatting and dependencies version

--- example/apollo_federation/gateway/index.js
+++ example/apollo_federation/gateway/index.js
@@ -4,8 +4,8 @@ const { ApolloGateway, IntrospectAndCompose } = require('@apollo/gateway');
 const gateway = new ApolloGateway({
     supergraphSdl: new IntrospectAndCompose({
         subgraphs: [
-            { name: 'one', url: 'http://localhost:4001' },
-            { name: 'two', url: 'http://localhost:4002' },
+            { name: 'one', url: 'http://localhost:4001/query' },
+            { name: 'two', url: 'http://localhost:4002/query' },
         ],
     }),
 });
