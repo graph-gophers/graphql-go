92a4068ba3ff5dcab91754134b15425e1cb91d94
Sean Sorrell <me@seansorrell.org>
2022-12-24 08:47:36 -0800

    more docs

--- directives/visitor.go
+++ directives/visitor.go
@@ -9,6 +9,9 @@ import (
 // Visitor defines the interface that clients should use to implement a Directive
 // see the graphql.DirectiveVisitors() Schema Option
 type Visitor interface {
+	// Before() is always called when the operation includes a directive matching this implementation's name
+	// Errors in Before() will prevent field resolution
 	Before(ctx context.Context, directive *types.Directive, input interface{}) error
+	// After is called if Before() *and* the field resolver do not error
 	After(ctx context.Context, directive *types.Directive, output interface{}) (interface{}, error)
 }
