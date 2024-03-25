b084162b306480614e46b64aed4274cf0c784ba7
David Ackroyd <23301187+dackroyd@users.noreply.github.com>
2024-03-26 10:50:46 +1100

    Upgrade graphql-js Testdata Process
    The process for pulling in test cases from graphql-js has been broken
    for a long time. The project has made an array of changes which prevent
    the existing approach from being functional, including migration to
    Typescript, changes to assertion helper functions and more.
    
    With this change, the approach for integration is shifted. Rather than
    needing to clone the graphql-js repo, instead we pull it in as a
    dependency of the Node.js project under testdata. This is patched via
    patch-package in a postinstall hook, which causes the test functions to
    write the cases out, and those are collected and written to JSON.
    
    As expected, the test cases have a significant number of updates, and
    the Go tests driven by them fail in their current state, so further work
    is required.
    
    Parity with test files pulled in from graphql-js prior to this change is
    maintained. A number of test files were excluded due to failures, and
    others have been introduced since: these are left alone for now, as it
    is expected that enabling those will require larger changes to the
    implementation to satisfy them.
    
    Tests that have been added manually to the tests.json file have also been
    abandoned here. The JSON file is generated entirely by the process defined,
    and additional test cases will need to be recovered and re-implemented in
    other ways if they are still valuable.

--- internal/validation/validation_test.go
+++ internal/validation/validation_test.go
@@ -4,6 +4,7 @@ import (
 	"os"
 	"reflect"
 	"sort"
+	"strings"
 	"testing"
 
 	"encoding/json"
@@ -25,6 +26,18 @@ type Test struct {
 }
 
 func TestValidate(t *testing.T) {
+	skip := map[string]struct{}{
+		// The meta schema always includes the standard types, so this isn't applicable
+		"Validate: Known type names/references to standard scalars that are missing in schema": {},
+		// Ignore tests using experimental @stream
+		"Validate: Overlapping fields can be merged/different stream directive label":               {},
+		"Validate: Overlapping fields can be merged/different stream directive initialCount":        {},
+		"Validate: Overlapping fields can be merged/different stream directive first missing args":  {},
+		"Validate: Overlapping fields can be merged/different stream directive second missing args": {},
+		"Validate: Overlapping fields can be merged/different stream directive extra argument":      {},
+		"Validate: Overlapping fields can be merged/mix of stream and no stream":                    {},
+	}
+
 	f, err := os.Open("testdata/tests.json")
 	if err != nil {
 		t.Fatal(err)
@@ -40,23 +53,39 @@ func TestValidate(t *testing.T) {
 
 	schemas := make([]*ast.Schema, len(testData.Schemas))
 	for i, schemaStr := range testData.Schemas {
-		schemas[i] = schema.New()
-		err := schema.Parse(schemas[i], schemaStr, false)
+		s := schema.New()
+
+		s.Directives["oneOf"] = &ast.DirectiveDefinition{
+			// graphql-js includes support for @oneOf, currently in RFC
+			// This is not available in graphql-go, nor is it expected to be unless the RFC is accepted
+			// See https://github.com/graphql/graphql-js/pull/3513 & https://github.com/graphql/graphql-spec/pull/825/
+			Name:      "oneOf",
+			Desc:      "Indicates exactly one field must be supplied and this field must not be `null`.",
+			Locations: []string{"INPUT_OBJECT"},
+		}
+
+		err := schema.Parse(s, schemaStr, false)
 		if err != nil {
 			t.Fatal(err)
 		}
+		schemas[i] = s
 	}
 
 	for _, test := range testData.Tests {
 		t.Run(test.Name, func(t *testing.T) {
+			if _, ok := skip[test.Name]; ok {
+				t.Skip("Test ignored")
+			}
+
 			d, err := query.Parse(test.Query)
 			if err != nil {
-				t.Fatal(err)
+				t.Fatalf("failed to parse query: %s", err)
 			}
 			errs := validation.Validate(schemas[test.Schema], d, test.Vars, 0)
 			got := []*errors.QueryError{}
 			for _, err := range errs {
-				if err.Rule == test.Rule {
+				// graphql-js adjusted its rule naming. Preserve the existing naming to minimise changes for now
+				if rule := strings.TrimSuffix(test.Rule, "Rule"); err.Rule == rule {
 					err.Rule = ""
 					got = append(got, err)
 				}
@@ -64,7 +93,7 @@ func TestValidate(t *testing.T) {
 			sortLocations(test.Errors)
 			sortLocations(got)
 			if !reflect.DeepEqual(test.Errors, got) {
-				t.Errorf("wrong errors\nexpected: %v\ngot:      %v", test.Errors, got)
+				t.Errorf("wrong errors for rule %q\nexpected: %v\ngot:      %v", test.Rule, test.Errors, got)
 			}
 		})
 	}
@@ -75,4 +104,7 @@ func sortLocations(errs []*errors.QueryError) {
 		locs := err.Locations
 		sort.Slice(locs, func(i, j int) bool { return locs[i].Before(locs[j]) })
 	}
+
+	// FIXME: Sorting errors seems necessary, as mutations being validated before queries, but order reversed in expectations?
+	sort.Slice(errs, func(i, j int) bool { return errs[i].Locations[0].Before(errs[j].Locations[0]) })
 }
