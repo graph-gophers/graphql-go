ea4b1e1b52ca29c9bdd02555a3a9e5d19b007eef
Mike Knowles <michaelknowles@users.noreply.github.com>
2025-09-15 22:16:20 -0700

    feat(meta): add description for Time

--- internal/schema/meta.go
+++ internal/schema/meta.go
@@ -38,6 +38,9 @@ var metaSrc = `
 	# The ` + "`" + `ID` + "`" + ` scalar type represents a unique identifier, often used to refetch an object or as key for a cache. The ID type appears in a JSON response as a String; however, it is not intended to be human-readable. When expected as an input type, any string (such as ` + "`" + `"4"` + "`" + `) or integer (such as ` + "`" + `4` + "`" + `) input value will be accepted as an ID.
 	scalar ID
 
+	# The ` + "`" + `Time` + "`" + ` scalar type represents a single moment in time. Time values are serialized as RFC3339 format strings and can be parsed from RFC3339 strings, Unix timestamps, or Unix timestamps with nanosecond precision.
+	scalar Time
+
 	# Directs the executor to include this field or fragment only when the ` + "`" + `if` + "`" + ` argument is true.
 	directive @include(
 		# Included when true.
