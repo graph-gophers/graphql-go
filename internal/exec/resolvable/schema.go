776ce5eba7ca5573ed543cd76f995853472974e9
Evan Owen <evan@gluegroups.com>
2025-11-12 14:28:46 -0800

    Fix uninitialized buffer pool for introspection query

--- internal/exec/resolvable/schema.go
+++ internal/exec/resolvable/schema.go
@@ -7,6 +7,8 @@ import (
 	"github.com/graph-gophers/graphql-go/ast"
 )
 
+var disabledBufferPool = newBufferPool(0)
+
 type Schema struct {
 	*Meta
 	ast.Schema
@@ -21,10 +23,19 @@ type Schema struct {
 }
 
 func (s *Schema) BufferPool() Pool[*bytes.Buffer] {
+	if s.bufferPool == nil {
+		return disabledBufferPool
+	}
+
 	return s.bufferPool
 }
 
 func newSchema(astSchema *ast.Schema, resolvers map[string]interface{}, query, mutation, subscription Resolvable, maxPooledBufferCap int) *Schema {
+	var bufferPool Pool[*bytes.Buffer]
+	if maxPooledBufferCap > 0 {
+		bufferPool = newBufferPool(maxPooledBufferCap)
+	}
+
 	return &Schema{
 		Meta:                 newMeta(astSchema),
 		Schema:               *astSchema,
@@ -35,6 +46,6 @@ func newSchema(astSchema *ast.Schema, resolvers map[string]interface{}, query, m
 		Mutation:             mutation,
 		Subscription:         subscription,
 
-		bufferPool: newBufferPool(maxPooledBufferCap),
+		bufferPool: bufferPool,
 	}
 }
